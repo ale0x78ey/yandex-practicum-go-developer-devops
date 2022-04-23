e5c26dfb057a6443a219a6c0209452b307897fa2
Alexey Ulyanov <ale0x78ey@yandex.ru>
2022-04-23 21:33:37 +0300

    add dsn d flag

--- config/config_db.go
+++ config/config_db.go
@@ -1,9 +1,13 @@
 package config
 
 import (
+	"flag"
+
 	"github.com/ale0x78ey/yandex-practicum-go-developer-devops/storage/db"
 )
 
 func NewDBConfig() *db.Config {
-	return &db.Config{}
+	cfg := &db.Config{}
+	flag.StringVar(&cfg.DSN, "d", "", "DATABASE_DSN")
+	return cfg
 }
