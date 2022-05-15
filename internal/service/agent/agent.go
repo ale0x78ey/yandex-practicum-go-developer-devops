f247b256ca305bba952de04d16a4fef81c41efc1
Alexey Ulyanov <ale0x78ey@yandex.ru>
2022-05-15 15:54:10 +0300

    pollMetrics and postMetrics in 2 goroutines

--- internal/service/agent/agent.go
+++ internal/service/agent/agent.go
@@ -47,18 +47,13 @@ func (c Config) Validate() error {
 	return nil
 }
 
-type metrics struct {
-	memStats    runtime.MemStats
-	randomValue float64
-	pollCount   int64
-}
-
 type Agent struct {
 	config    Config
 	client    *resty.Client
-	data      metrics
-	wg        sync.WaitGroup
 	updateURL string
+	metrics   chan model.Metric
+
+	pollCount int64
 }
 
 func NewAgent(config Config, updateURL string) (*Agent, error) {
@@ -84,92 +79,111 @@ func NewAgent(config Config, updateURL string) (*Agent, error) {
 		config:    config,
 		client:    client,
 		updateURL: updateURL,
+		metrics:   make(chan model.Metric),
 	}
 
 	return a, nil
 }
 
 func (a *Agent) Run(ctx context.Context) error {
-	pollTicker := time.NewTicker(a.config.PollInterval)
-	defer pollTicker.Stop()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	defer wg.Wait()
 
-	sendTicker := time.NewTicker(a.config.ReportInterval)
-	defer sendTicker.Stop()
+	go a.pollMetrics(ctx, wg)
+	go a.postMetrics(ctx, wg)
+
+	return nil
+}
+
+func (a *Agent) pollMemStatsMetrics() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	a.metrics <- model.MetricFromGauge("Alloc", model.Gauge(m.Alloc))
+	a.metrics <- model.MetricFromGauge("TotalAlloc", model.Gauge(m.TotalAlloc))
+	a.metrics <- model.MetricFromGauge("BuckHashSys", model.Gauge(m.BuckHashSys))
+	a.metrics <- model.MetricFromGauge("Frees", model.Gauge(m.Frees))
+	a.metrics <- model.MetricFromGauge("GCCPUFraction", model.Gauge(m.GCCPUFraction))
+	a.metrics <- model.MetricFromGauge("GCSys", model.Gauge(m.GCSys))
+	a.metrics <- model.MetricFromGauge("HeapAlloc", model.Gauge(m.HeapAlloc))
+	a.metrics <- model.MetricFromGauge("HeapIdle", model.Gauge(m.HeapIdle))
+	a.metrics <- model.MetricFromGauge("HeapInuse", model.Gauge(m.HeapInuse))
+	a.metrics <- model.MetricFromGauge("HeapObjects", model.Gauge(m.HeapObjects))
+	a.metrics <- model.MetricFromGauge("HeapReleased", model.Gauge(m.HeapReleased))
+	a.metrics <- model.MetricFromGauge("HeapSys", model.Gauge(m.HeapSys))
+	a.metrics <- model.MetricFromGauge("LastGC", model.Gauge(m.LastGC))
+	a.metrics <- model.MetricFromGauge("Lookups", model.Gauge(m.Lookups))
+	a.metrics <- model.MetricFromGauge("MCacheInuse", model.Gauge(m.MCacheInuse))
+	a.metrics <- model.MetricFromGauge("MCacheSys", model.Gauge(m.MCacheSys))
+	a.metrics <- model.MetricFromGauge("MSpanInuse", model.Gauge(m.MSpanInuse))
+	a.metrics <- model.MetricFromGauge("MSpanSys", model.Gauge(m.MSpanSys))
+	a.metrics <- model.MetricFromGauge("Mallocs", model.Gauge(m.Mallocs))
+	a.metrics <- model.MetricFromGauge("NextGC", model.Gauge(m.NextGC))
+	a.metrics <- model.MetricFromGauge("NumForcedGC", model.Gauge(m.NumForcedGC))
+	a.metrics <- model.MetricFromGauge("NumGC", model.Gauge(m.NumGC))
+	a.metrics <- model.MetricFromGauge("OtherSys", model.Gauge(m.OtherSys))
+	a.metrics <- model.MetricFromGauge("PauseTotalNs", model.Gauge(m.PauseTotalNs))
+	a.metrics <- model.MetricFromGauge("StackInuse", model.Gauge(m.StackInuse))
+	a.metrics <- model.MetricFromGauge("StackSys", model.Gauge(m.StackSys))
+	a.metrics <- model.MetricFromGauge("Sys", model.Gauge(m.Sys))
+}
+
+func (a *Agent) pollRandomValueMetric() {
+	randomValue := rand.Float64()
+	a.metrics <- model.MetricFromGauge("RandomValue", model.Gauge(randomValue))
+}
+
+func (a *Agent) pollCountMetric() {
+	a.pollCount++
+	a.metrics <- model.MetricFromCounter("PollCount", model.Counter(a.pollCount))
+}
+
+func (a *Agent) pollMetrics(ctx context.Context, wg *sync.WaitGroup) {
+	ticker := time.NewTicker(a.config.PollInterval)
+	defer ticker.Stop()
+	defer wg.Done()
 
 	for {
 		select {
-		case <-pollTicker.C:
-			a.pollMetrics()
-		case <-sendTicker.C:
-			a.postMetrics(ctx)
 		case <-ctx.Done():
-			return nil
+			return
+		case <-ticker.C:
+			a.pollMemStatsMetrics()
+			a.pollRandomValueMetric()
+			a.pollCountMetric()
 		}
 	}
 }
 
-func (a *Agent) pollMetrics() {
-	runtime.ReadMemStats(&a.data.memStats)
-	a.data.randomValue = rand.Float64()
-	a.data.pollCount++
+func (a *Agent) postOneMetric(ctx context.Context, metric model.Metric) error {
+	if err := metric.UpdateHash(a.config.Key); err != nil {
+		return err
+	}
+
+	_, err := a.client.R().
+		SetContext(ctx).
+		SetBody(metric).
+		Post(a.updateURL)
+
+	return err
 }
 
-func (a *Agent) postMetrics(ctx context.Context) {
-	postTimeout := minDuration(a.config.ReportInterval, a.config.PollInterval)
-	ctx, cancel := context.WithTimeout(ctx, postTimeout)
-	defer cancel()
+func (a *Agent) postMetrics(ctx context.Context, wg *sync.WaitGroup) {
+	ticker := time.NewTicker(a.config.ReportInterval)
+	defer ticker.Stop()
+	defer wg.Done()
 
-	m := &a.data.memStats
-
-	a.post(ctx, model.MetricFromGauge("Alloc", model.Gauge(m.Alloc)))
-	a.post(ctx, model.MetricFromGauge("TotalAlloc", model.Gauge(m.TotalAlloc)))
-	a.post(ctx, model.MetricFromGauge("BuckHashSys", model.Gauge(m.BuckHashSys)))
-	a.post(ctx, model.MetricFromGauge("Frees", model.Gauge(m.Frees)))
-	a.post(ctx, model.MetricFromGauge("GCCPUFraction", model.Gauge(m.GCCPUFraction)))
-	a.post(ctx, model.MetricFromGauge("GCSys", model.Gauge(m.GCSys)))
-	a.post(ctx, model.MetricFromGauge("HeapAlloc", model.Gauge(m.HeapAlloc)))
-	a.post(ctx, model.MetricFromGauge("HeapIdle", model.Gauge(m.HeapIdle)))
-	a.post(ctx, model.MetricFromGauge("HeapInuse", model.Gauge(m.HeapInuse)))
-	a.post(ctx, model.MetricFromGauge("HeapObjects", model.Gauge(m.HeapObjects)))
-	a.post(ctx, model.MetricFromGauge("HeapReleased", model.Gauge(m.HeapReleased)))
-	a.post(ctx, model.MetricFromGauge("HeapSys", model.Gauge(m.HeapSys)))
-	a.post(ctx, model.MetricFromGauge("LastGC", model.Gauge(m.LastGC)))
-	a.post(ctx, model.MetricFromGauge("Lookups", model.Gauge(m.Lookups)))
-	a.post(ctx, model.MetricFromGauge("MCacheInuse", model.Gauge(m.MCacheInuse)))
-	a.post(ctx, model.MetricFromGauge("MCacheSys", model.Gauge(m.MCacheSys)))
-	a.post(ctx, model.MetricFromGauge("MSpanInuse", model.Gauge(m.MSpanInuse)))
-	a.post(ctx, model.MetricFromGauge("MSpanSys", model.Gauge(m.MSpanSys)))
-	a.post(ctx, model.MetricFromGauge("Mallocs", model.Gauge(m.Mallocs)))
-	a.post(ctx, model.MetricFromGauge("NextGC", model.Gauge(m.NextGC)))
-	a.post(ctx, model.MetricFromGauge("NumForcedGC", model.Gauge(m.NumForcedGC)))
-	a.post(ctx, model.MetricFromGauge("NumGC", model.Gauge(m.NumGC)))
-	a.post(ctx, model.MetricFromGauge("OtherSys", model.Gauge(m.OtherSys)))
-	a.post(ctx, model.MetricFromGauge("PauseTotalNs", model.Gauge(m.PauseTotalNs)))
-	a.post(ctx, model.MetricFromGauge("StackInuse", model.Gauge(m.StackInuse)))
-	a.post(ctx, model.MetricFromGauge("StackSys", model.Gauge(m.StackSys)))
-	a.post(ctx, model.MetricFromGauge("Sys", model.Gauge(m.Sys)))
-	a.post(ctx, model.MetricFromGauge("RandomValue", model.Gauge(a.data.randomValue)))
-	a.post(ctx, model.MetricFromCounter("PollCount", model.Counter(a.data.pollCount)))
-
-	a.wg.Wait()
-}
-
-func (a *Agent) post(ctx context.Context, metric model.Metric) {
-	a.wg.Add(1)
-	go func() {
-		defer a.wg.Done()
-
-		if err := metric.UpdateHash(a.config.Key); err != nil {
-			log.Printf("failed to post %v: %v", metric, err)
+	for {
+		select {
+		case <-ctx.Done():
 			return
+		case <-ticker.C:
+			for metric := range a.metrics {
+				if err := a.postOneMetric(ctx, metric); err != nil {
+					log.Printf("failed to post %v: %v", metric, err)
+				}
+			}
 		}
-
-		_, err := a.client.R().
-			SetContext(ctx).
-			SetBody(metric).
-			Post(a.updateURL)
-		if err != nil {
-			log.Printf("failed to post %v: %v", metric, err)
-		}
-	}()
+	}
 }
